98ff9f137bb8b2d3a016f778a1e1577140459ee8
Andre Ravazzi <amravazzi@gmail.com>
2021-09-28 00:04:56 -0300

    batman

---
+++ terraform/1-locals.tf
@@ -0,0 +1,14 @@
+locals {
+  region                = "us-central1-c"
+  org_id                = "652770409937"
+  billing_account       = "015C14-181A4D-D47ED0"
+  host_project_name     = "host-staging"
+  service_project_name  = "host-staging"
+  host_project_id       = "${local.host_project_name}-${random_integer.int.result}"
+  service_project_id    = "${local.service_project_name}-${random_integer.int.result}"
+  projects_api          = "container.googleapis.com"
+  secondary_ip_ranges   = {
+    "pod-ip-range"      = "10.0.0.0/14",
+    "service-ip-range"  = "10.0.0.0/19"
+  }
+}
